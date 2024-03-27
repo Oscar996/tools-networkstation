45d4f3d343ea1218f0fd9d36ad26c6d231d80608
Oscar996 <72895074+Oscar996@users.noreply.github.com>
2024-03-27 11:20:56 -0400

    Update tool-network.sh

--- tool-network.sh
+++ tool-network.sh
@@ -4,13 +4,14 @@ sudo apt upgrade
 sudo apt update
 
 #Instalação das aplicações
-sudo apt install nmap neofetch snmp speedtest-cli ipcalc tcping traceroute
+sudo apt install nmap neofetch snmp speedtest-cli ipcalc traceroute
 
 #Instalação Blacklist check
 git clone https://github.com/adionditsak/blacklist-check-unix-linux-utility.git
 cd blacklist-check-unix-linux-utility
 sudo chmod +x ./bl
 sudo mv ./bl /usr/local/bin
+cd ~
 
 #Personalização NeoFetch
 cp /home/$USER/.bashrc
