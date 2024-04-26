0551ab486e067cc00fbac1c0775059ef6e89d1e9
fabioew89 <fabioew89@gmail.com>
2024-04-26 11:08:29 -0400

    modified:   network_tools.sh

--- network_tools.sh
+++ network_tools.sh
@@ -1,82 +1,101 @@
 #!/usr/bin/env bash
 
-##### ##### ##### ##### ##### VARIABLES ##### ##### ##### ##### ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
+#####                         VARIABLES                         ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
 
-RED="\e[31;1m"
+# RED="\e[31;1m"
 GREEN="\e[32;1m"
 YELLOW="\e[33;1m"
-BLUE="\e[34;1m"
+# BLUE="\e[34;1m"
 RESET="\e[0m"
 
+SLEEP="sleep 2"
+
 APT_UPDATE_LIST=(
-    "update --fix-missing"
-    "upgrade -y"
+    "update"
+    "upgrade"
     "autoclean"
     "autoremove"
 )
 
 APT_INSTALL_PACKAGES=(
-    git
-    vim
+    aha
+    bind9-host
     curl
-    zsh
     flatpak
-    nmap
+    git
+    grepcidr
+    ipcalc
+    jq
+    mtr
+    mtr
+    ncat
     neofetch
+    nmap
     snmp
     speedtest-cli
-    ipcalc
     traceroute
-    mtr
+    vim
     whois
-    curl
-    bind9-host
-    mtr-tiny
-    jq
-    grepcidr
-    ncat 
-    aha    
+    zsh  
 )
 
-##### ##### ##### ##### ##### ACTION ##### ##### ##### ##### ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
+#####                         ACTION                            ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
 
 apt_install_updates(){ #updates ?
+    echo -e "${GREEN}[INFO] - Updating...\n${RESET}"
     for update in "${APT_UPDATE_LIST[@]}"; do
-        sudo apt "$update" > /dev/null 2>&1
+        sudo apt "$update" -y > /dev/null 2>&1
+        $SLEEP
     done
-    echo -e "${GREEN}[INFO] - Sistema atualizado...${RESET}"
 }
 
-apt_install_packages(){ # apt install apps?
+apt_install_packages(){ # install apps?
     for package in "${APT_INSTALL_PACKAGES[@]}"; do
-        if ! which "$package" > /dev/null 2>&1; then
+        if ! dpkg -l | grep -qw "^ii\s\+$package" > /dev/null 2>&1; then # instaled?
+           
             echo -e "${YELLOW}[INFO] - Instalando $package${RESET}"
             sudo apt install "$package" -y > /dev/null 2>&1
 
         else
             echo -e "${GREEN}[INFO] - $package já instalado${RESET}"
         fi
+
+        $SLEEP
     done
 }
+
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
+#####                         CALL TO ACTION                    ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
 
 apt_install_updates
 apt_install_packages
 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
+#####                         OTHERS                            ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
+
 #Neofetch
 echo 'alias neo="neofetch"' >> "$HOME"/.bashrc
+source "$HOME"/.bashrc
 echo neofetch >> "$HOME"/.bashrc
 
 #Instalação ASN
 curl "https://raw.githubusercontent.com/nitefood/asn/master/asn" > asn && \
 sudo chmod 0755 asn 
-sudo cp asn /usr/bin/asn
+sudo mv -f asn /usr/bin/asn
 
 #Blacklist
 git clone https://github.com/adionditsak/blacklist-check-unix-linux-utility.git
 sudo cp blacklist-check-unix-linux-utility/bl /usr/bin/bl
 
-
-##### ##### ##### ##### ##### END ##### ##### ##### ##### ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
+#####                         END                               ##### 
+##### ##### ##### ##### ##### ##### ### ##### ##### ##### ##### ##### 
 
 # #Validação
 # echo "Tudo certo meu patrão!!!"
